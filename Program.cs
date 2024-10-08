c29b519607b1762d74a181f51c8fa9962db9fa55
Christofer Carlsson <113172285+ChristoferCarlsson@users.noreply.github.com>
2024-10-08 13:09:16 +0200

    Set up the project to try out the github

--- Program.cs
+++ Program.cs
@@ -4,8 +4,42 @@
     {
         static void Main(string[] args)
         {
-            bool running = true;
+            Console.WriteLine("Välkommen!");
 
+            bool running = true;
+            while (running) { 
+
+                Console.WriteLine("1. Lägg till en bok");
+                Console.WriteLine("2. Ta bort en bok");
+                Console.WriteLine("3. Sök efter en bok enligt författare");
+                Console.WriteLine("4. Visa alla böcker");
+                Console.WriteLine("5. Checka ut/returnera bok");
+                Console.WriteLine("6. Avsluta");
+
+                string input = Console.ReadLine();
+                switch (input) {
+
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Jag förstår inte");
+                        break;
+            
+                }
+
+            }
         }
     }
 }
