471a4c06927719b9bb245aa19f5c6d25eea754a8
sidali <sidaliml01@gmail.com>
2024-10-15 11:28:43 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace LibraryGroup
 {
@@ -8,22 +11,26 @@ namespace LibraryGroup
         {
             Console.WriteLine("Välkommen!");
 
-            //Vi skapar biblioteket
+            // Vi skapar biblioteket
             Library library = new Library();
 
             bool running = true;
-            while (running) { 
-
+            while (running)
+            {
+                Console.WriteLine("\nMeny:");
                 Console.WriteLine("1. Lägg till en bok");
                 Console.WriteLine("2. Ta bort en bok");
                 Console.WriteLine("3. Sök efter en bok enligt författare");
                 Console.WriteLine("4. Visa alla böcker");
-                Console.WriteLine("5. Checka ut/returnera bok");
-                Console.WriteLine("6. Avsluta");
+                Console.WriteLine("5. Checka ut en bok ");
+                Console.WriteLine("6. Returnera en bok");
+                Console.WriteLine("7. Avsluta");
 
                 string input = Console.ReadLine();
-                switch (input) {
+                switch (input)
+                {
                     case "1":
+                        // Lägg till en bok
                         Console.WriteLine("Vad heter boken?");
                         string title = Console.ReadLine();
 
@@ -36,47 +43,61 @@ namespace LibraryGroup
                         library.CreateBook(title, author, isbn);
 
                         Console.WriteLine("Boken är nu tillagd!");
-                        //Ge lite väntetid för effekt
-                        Thread.Sleep(1000);
+                        Thread.Sleep(1000); // Ger lite väntetid för effekt
                         break;
 
                     case "2":
-                        Console.WriteLine("Ange titel på den bok du vill ta bort från biblioteket: ");
-                        string bookToRemove = Console.ReadLine();
-                        library.RemoveBook(bookToRemove);
-                        Thread.Sleep(1000);//Ge lite väntetid för effekt
+                        // Ta bort en bok
+                        Console.WriteLine("Ange titeln på boken du vill ta bort:");
+                        string removeTitle = Console.ReadLine();
+                        library.RemoveBook(removeTitle); // Anropar metoden för att ta bort boken
                         break;
+
                     case "3":
-                        Console.WriteLine("Ange bokens författare");
+                        // Sök efter en bok enligt författare
+                        Console.WriteLine("Ange bokens författare:");
                         string userSearchAuthor = Console.ReadLine();
                         List<Book> foundBook = library.SearchBookByAuthor(userSearchAuthor);
                         if (foundBook != null && foundBook.Count > 0)
                         {
                             foreach (Book book in foundBook)
                             {
-                                Console.Write("Följande bok matchade din sökning - ");
                                 Console.WriteLine($"Titel: {book.Title}, Författare: {book.Author}, ISBN: {book.ISBN}");
                             }
                         }
                         else
                         {
-                            Console.WriteLine("Ingen bok matchade din sökning");
+                            Console.WriteLine("Ingen bok matchade din sökning.");
                         }
                         break;
+
                     case "4":
+                        // Visa alla böcker
+                        library.ShowAllBooks();
                         break;
+
                     case "5":
+                        // Checka ut en bok (not available)
+                        Console.WriteLine("Funktionen 'Checka ut en bok' not available.");
                         break;
+
                     case "6":
+                        // Returnera en bok
+                        Console.WriteLine("Ange bokens ISBN för att returnera:");
+                        string returnIsbn = Console.ReadLine();
+                        library.ReturnBook(returnIsbn); // Anropar metoden för att returnera boken
+                        break;
+
+                    case "7":
+                        // Avsluta programmet
                         running = false;
+                        Console.WriteLine("Avslutar programmet...");
                         break;
 
                     default:
-                        Console.WriteLine("Jag förstår inte");
+                        Console.WriteLine("Jag förstår inte, försök igen.");
                         break;
-            
                 }
-
             }
         }
     }
